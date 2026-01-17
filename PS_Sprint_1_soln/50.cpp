eeae8cb3e5fe90e87c7fb221b8ba622dad00f470
tanmoyPaul12 <tanmoy01paul@gmail.com>
2026-01-17 13:53:47 +0530

    10ques

--- PS_Sprint_1_soln/50.cpp
+++ PS_Sprint_1_soln/50.cpp
@@ -125,3 +125,126 @@
 // }
 
 // 6. **Identifying Palindromes**  
+
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+
+// int main(){
+//     string s, rev;
+//     cout<<"Enter"<<endl;
+//     cin>>s;
+//     rev = s;
+//     reverse(rev.begin(), rev.end());
+
+//     if(s==rev){
+//         cout<<"Palindrome";
+//     }
+//     else{
+//         cout<<"Not palindrome";
+//     }
+//     return 0;
+// }
+
+// ==============With only Strings================
+
+// #include<iostream>
+// #include<string>
+// using namespace  std;
+
+// int main(){
+//     string s;
+//     cout<<"Enter string"<<endl;
+//     cin>>s;
+
+//     int start = 0;
+//     int end = s.length()-1;
+
+//     while(start < end){
+//         if(s[start]!= s[end]){
+//             cout<<"Not Palindrome";
+//             break;
+//         }
+//         else{
+//             cout<<"Palindrome";
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// 7. **Crafting Star Patterns**  
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int height = 5;
+//     for(int i=1; i<=height; i++){
+        
+//         for(int space=1; space<=height-1; space++)
+//         cout<<" ";
+
+//         for(int star=1; star<=(2*i-1); star++)
+//         cout<<"*";
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// 8. **Finding the Factorial of a Number**  
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     long long fact=1;
+//     cout<<"Enter a number"<<endl;
+//     cin>>n;
+    
+//     for(int i=1; i<=n; i++){
+//         fact = fact*i;
+//     }
+
+//     cout<<fact;
+//     return 0 ;
+// }
+
+// 9. **Summing Digits of a Number**  
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n, digits, sum = 0;
+//     cout<<"Enter a number"<<endl;
+//     cin>>n;
+
+//     while(n>0){
+//         digits= n% 10;
+//         sum= sum + digits;
+//         n= n/10;
+//     }
+//     cout<<sum;
+//     return 0;
+// }
+
+// 10. **Finding the Greatest Common Divisor (GCD)**  
+// =============EUCLIDIAN METHOD=====================
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a , b;
+//     cout<<"Enter 2 numbers"<<endl;
+//     cin>>a>>b;
+
+//     while(b!=0){
+//         int temp = b;
+//         b = a%b;
+//         a=temp;
+//     }
+//     cout<<a;
+//     return 0;
+// }
